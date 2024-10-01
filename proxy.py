32a4ae8b3e92cf1e36a1cbdecdd5437b03a1a792
Nils Schimmelmann <nschimme@users.noreply.github.com>
2024-10-01 12:34:02 -0500

    Update proxy.py
    Update script with https://github.com/nschimme/ollama-proxy/issues/1

--- proxy.py
+++ proxy.py
@@ -9,40 +9,40 @@ import sys
 TARGET_URL = os.getenv('TARGET_URL', 'http://192.168.0.200:11434')
 
 MODEL_SWAP = {
-    'codestral': 'codestral:latest',
+    'codestral': 'codestral:22b-v0.1-q4_K_M',
     'phi3': 'phi3:14b-medium-4k-instruct-q4_K_M',
     'llama3': 'llama3:8b-instruct-q8_0',
     'llama3:8b': 'llama3:8b-instruct-q8_0',
     'codeqwen:code': 'codeqwen:7b-code-v1.5-q8_0',
-    'starcoder2': 'starcoder2:15b'
+    'starcoder2': 'starcoder2:15b',
+    'starcoder2:3b': 'starcoder2:15b'
 }
 
 async def handle_request(request):
     try:
         method = request.method
         path = request.path_qs
-        headers = {key: value for key, value in request.headers.items()}
-
+        headers = {key: value for key, value in request.headers.items() if key.lower() not in {'content-length'}}
         if request.can_read_body:
             post_data = await request.read()
         else:
             post_data = None
-
         if post_data:
             try:
                 json_data = json.loads(post_data)
-                if "model" in json_data:
-                    swap_key = json_data["model"]
-                    print(f"json model: {swap_key}")
-                    if swap_key in MODEL_SWAP:
-                        print("replacing {} with {}".format(swap_key, MODEL_SWAP[swap_key]))
-                        json_data["model"] = MODEL_SWAP[swap_key]
-                        post_data = json.dumps(json_data).encode('utf-8')
-                        headers['Content-Length'] = str(len(post_data))
+                if "model" in json_data and json_data["model"] in MODEL_SWAP:
+                    print("replacing {} with {}".format(json_data["model"], MODEL_SWAP[json_data["model"]]))
+                    json_data["model"] = MODEL_SWAP[json_data["model"]]
+                del_keys = ["raw", "options","keep_alive"]
+                for dkey in del_keys:
+                    if dkey in json_data:
+                        del json_data[dkey]
+                post_data = json.dumps(json_data).encode('utf-8')
             except json.JSONDecodeError:
                 return Response(status=400, text="Bad Request: Invalid JSON")
-
+            print(post_data)
         async with ClientSession() as session:
+            # print(method, " ", TARGET_URL, path)
             async with session.request(method, TARGET_URL + path, headers=headers, data=post_data) as response:
                 stream_response = StreamResponse(status=response.status, reason=response.reason)
                 for key, value in response.headers.items():
@@ -53,13 +53,23 @@ async def handle_request(request):
                 async for chunk in response.content.iter_any():
                     await stream_response.write(chunk)
                 await stream_response.write_eof()
-
                 return stream_response
-
     except ClientResponseError as e:
         return Response(status=e.status, text=str(e))
     except Exception as e:
         return Response(status=500, text=str(e))
+
+async def list_models():
+    async with ClientSession() as session:
+        async with session.request('GET', TARGET_URL + '/api/tags') as response:
+            return await response.json()
+
+def print_table(data):
+    col_widths = [max(len(str(row[i])) for row in data) + 2 for i in range(len(data[0]))]
+    print(''.join(str(item).ljust(col_widths[i]) for i, item in enumerate(data[0])))
+    print('-' * (sum(col_widths) + 1))
+    for row in data[1:]:
+        print(''.join(str(item).ljust(col_widths[i]) for i, item in enumerate(row)))
 
 async def create_app():
     app = Application()
@@ -67,6 +77,22 @@ async def create_app():
     return app
 
 if __name__ == '__main__':
+    # print ollama models available
+    available_models = set()
+    models = asyncio.run(list_models())
+    if 'models' in models:
+        m = [["Name", "Size"]]
+        for model in models['models']:
+            size_str = "{:.2f} GB".format(float(model['size'])/1073741824.0)
+            m.append([model['model'],size_str])
+            available_models.add(model['model'])
+        print_table(m)
+    # check and warn if replacement is to a non-existent model
+    for key, value in MODEL_SWAP.items():
+        if value not in available_models:
+            print(f"WARNING: MODEL_SWAP contains a non-existent model: {key} -> {value}")
+    
+    # run aiohttp proxy
     app = create_app()
     run_app(app, host='0.0.0.0', port=11434)
 
